735c4fbcd60925f9333574ce5a0a6f7c3b04a8e3
zeroj-0 <syj990930@naver.com>
2022-08-24 18:25:07 +0900

    syj update

--- bookie/src/main/java/com/teamecho/bookie/test/web/CreateExamController.java
+++ bookie/src/main/java/com/teamecho/bookie/test/web/CreateExamController.java
@@ -1,13 +1,99 @@
 package com.teamecho.bookie.test.web;
 
+import com.teamecho.bookie.common.domain.Category;
+import com.teamecho.bookie.common.domain.CategoryCommand;
+import com.teamecho.bookie.common.domain.CategoryProvider;
+import com.teamecho.bookie.common.service.CategoryService;
+import com.teamecho.bookie.common.service.SubjectPatternService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller("test.web.createExamController")
 public class CreateExamController {
 
+    @Autowired
+    CategoryService cateService;
+    @Autowired
+    SubjectPatternService spService;
+
     @GetMapping("/test/createExam")
-    public String createExamPage() {
+    public String createExamPage(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        model.addAttribute("category", new CategoryCommand());
         return "/test/create_exam";
+    }
+
+    @ModelAttribute("categoryProviderList")
+    public List<CategoryProvider> getLevelProviderList() {
+        List<CategoryProvider> list = new ArrayList<CategoryProvider>();
+        list.add(new CategoryProvider("중학생", "m"));
+        list.add(new CategoryProvider("고등학생", "h"));
+        return list;
+    }
+
+    @ModelAttribute("gradeProviderList")
+    public List<CategoryProvider> getGradeProviderList() {
+        List<CategoryProvider> list = new ArrayList<CategoryProvider>();
+        list.add(new CategoryProvider("1학년", "1"));
+        list.add(new CategoryProvider("2학년", "2"));
+        list.add(new CategoryProvider("3학년", "3"));
+        return list;
+    }
+    @ModelAttribute("subjectProviderList")
+    public List<CategoryProvider> getSubjectProviderList() {
+        List<CategoryProvider> list = new ArrayList<CategoryProvider>();
+        list.add(new CategoryProvider("국어", "국어"));
+        list.add(new CategoryProvider("수학", "수학"));
+        list.add(new CategoryProvider("영어", "영어"));
+        return list;
+    }
+
+    Category category;
+    String bigTag;
+    @GetMapping("test/createExamSelectPattern")
+    public String selectPattern(HttpServletRequest request, Model model, @ModelAttribute("category") CategoryCommand command) {
+        model.addAttribute("category", command);
+
+        category = cateService.getCategory(command.getCLevel(), command.getGrade(), command.getSubject());
+        // 카테아이디를 통해 대분류를 각각 태그에 담아서 해당 String을 다시 던져주기(이때 각 태그에는 중분류를 찾는 스트립트함수 호출하는 내용 들어가야 함)
+        List<String> bigPatterns = spService.getBigPatternsPatternsByCateId(category.getCateId());
+
+        bigTag = "";
+        for(int i = 0; i < bigPatterns.size(); i++) {
+            String big = bigPatterns.get(i);
+            bigTag += "<option value='" + big + "'>" + big + "</option>";
+
+        }
+
+        model.addAttribute("bigTag", bigTag);
+        return "test/create_exam";
+    }
+
+    @GetMapping("test/createExamSelectPattern_m")
+    public String selectPatternM(HttpServletRequest request, Model model){
+        String midTag = "";
+        String bigPattern = request.getParameter("bp");
+
+        List<String> midPatterns = spService.getMidPatternsByBigPatternAndCateId(bigPattern, category.getCateId());
+        for(int i = 0; i < midPatterns.size(); i++) {
+            String mid = midPatterns.get(i);
+            midTag += "<option value='" + mid + "'>" + mid +  "</option>";
+        }
+
+        model.addAttribute("midTag", midTag);
+        model.addAttribute("category", category);
+        model.addAttribute("bigPattern", bigPattern);
+        model.addAttribute("bigTag", bigTag);
+        return "test/create_exam";
     }
 }
--- bookie/src/main/webapp/WEB-INF/views/test/create_exam.jsp
+++ bookie/src/main/webapp/WEB-INF/views/test/create_exam.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -9,6 +10,7 @@
     <link rel="stylesheet" href="<c:url value="/resources/css/footer.css"/>" />
     <link rel="stylesheet" href="<c:url value="/resources/css/home/home.css"/>" />
     <link rel="stylesheet" href="<c:url value="/resources/css/question/question_solve.css"/>" />
+    <link rel="stylesheet" href="<c:url value="/resources/css/test/test_page.css"/>" />
 </head>
 <body>
 <div class="wrap sub">
@@ -16,8 +18,8 @@
         <%@ include file="../incl/header.jsp"%>
     </header>
     <div class="container counts solve">
-        <div class="category-wrap">
-            <form:form method="get" modelAttribute="category" action="/bookie/question/solveProblem">
+        <div class="category-wrap testcate">
+            <form:form method="get" modelAttribute="category" action="/bookie/test/createExamSelectPattern">
                 <form:select path="cLevel" class="selec">
                     <form:options itemValue="categoryHost" itemLabel="categoryCode" items="${categoryProviderList }" class="opt"/>
                 </form:select>
@@ -27,10 +29,66 @@
                 <form:select path="subject" class="selec">
                     <form:options itemValue="categoryHost" itemLabel="categoryCode" items="${subjectProviderList }" class="opt"/>
                 </form:select>
-                <input type="submit" value="문제풀기" class="submit-btn search solve"/>
+                <input type="submit" value="카테고리 선택" class="submit-btn search solve"/>
             </form:form>
+            <div class="subjectPattern-wrap">
+                <select id='bPattern' name='bPattern' onChange='javascript:checkingMPattern(this);' class="selec">
+                    <option value="unknown" class="opt">---대분류---</option>
+                    ${bigTag}
+                </select>
+                <select id='mPattern' name='mPattern' class="selec">
+                    <option value="unknown" class="opt">---중분류---</option>
+                    ${midTag}
+                </select>
+                <input type="button" value="선택" class="submit-btn search"/>
+            </div>
         </div>
     </div>
 </div>
+<footer id="footer">
+    <%@ include file="../incl/footer.jsp"%>
+</footer>
+<script>
+    <c:if test="${not empty category}">
+
+        let bp = document.querySelector('#bPattern');
+        for(let i = 0; i < bp.length; i++) {
+            if(bp.options[i].value === "${bigPattern}") {
+                bp.options[i].selected = true;
+                break;
+            }
+        }
+    </c:if>
+
+    function checkingMPattern(value) {
+        let sCheck = document.getElementById("bPattern");
+        let bpCheck = sCheck.options[sCheck.selectedIndex].value;
+        if(bpCheck == "unknown") {
+            return alert("대분류를 선택해주세요.");
+        }
+
+        let url = "createExamSelectPattern_m";
+        // 폼 생성
+        let form = document.createElement('form');
+        form.setAttribute('type', 'hidden');
+        form.setAttribute('method', 'get');
+        form.setAttribute('action', url);
+        document.charset = "UTF-8";
+
+        // 대분류
+        let s = document.getElementById("bPattern");
+        let bp = s.options[s.selectedIndex].value;
+
+        let input1 = document.createElement('input');
+        input1.setAttribute('type', 'hidden');
+        input1.setAttribute('name', 'bp');
+        input1.setAttribute('value', bp);
+        form.appendChild(input1);
+
+        // 폼 추가
+        document.body.appendChild(form);
+        form.submit();
+    }
+</script>
 </body>
 </html>
